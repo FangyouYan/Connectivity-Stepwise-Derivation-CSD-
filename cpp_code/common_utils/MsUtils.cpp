ac06522852061604b5665ba7a32ddb2ebba0fe1b
codeking <jialiang_xiong@163.com>
2024-04-30 13:21:55 +0800

    polish cpp code about CSD
    Signed-off-by: codeking <jialiang_xiong@163.com>

--- cpp_code/common_utils/MsUtils.cpp
+++ cpp_code/common_utils/MsUtils.cpp
@@ -4,6 +4,7 @@
 
 #include "MsUtils.h"
 #include <cfloat>
+#include <set>
 
 
 std::tuple<int, int, int> betterFastStrFromMol(const std::vector<std::string> &ISI) {
@@ -26,14 +27,40 @@ FastStepBondFromMol(int n_atom, int n_atom_start, int n_adj, const std::vector<s
         int i_s = std::min(num1 - 1, num2 - 1);
         int i_g = std::max(num1 - 1, num2 - 1);
         Sa[i_s][i_g] = 1.0;
-        Sa[i_g][i_s]= 1.0;
+        Sa[i_g][i_s] = 1.0;
     }
     return {Sa};
 }
 
+#include <unordered_map>
+
+// get adjacency matrix and atom connectivity information
+std::tuple<Matrix, std::unordered_map<int, std::set<int>>, std::set<std::pair<int, int>>>
+getMSaAndLadj(int n_atom, int n_atom_start, int n_adj, const std::vector<std::string> &ISI) {
+    // init Sa matrix,it is adjacency matrix
+    Matrix Sa(n_atom, std::vector<float>(n_atom, 0.0));
+    // Ladj
+    std::unordered_map<int, std::set<int>> Ladj;
+
+    std::set<std::pair<int, int>> step_xy;
+    for (int j = n_atom_start + n_atom; j < n_atom_start + n_atom + n_adj; ++j) {
+        const std::string &row = ISI[j];
+        int i_s = std::stoi(row.substr(0, 3)) - 1;
+        int i_g = std::stoi(row.substr(3, 3)) - 1;
+        Sa[i_s][i_g] = 1.0;
+        Sa[i_g][i_s] = 1.0;
+        Ladj[i_s + 1].insert(i_g + 1);
+        Ladj[i_g + 1].insert(i_s + 1);
+        step_xy.insert(std::make_pair(i_s + 1, i_g + 1));
+        step_xy.insert(std::make_pair(i_g + 1, i_s + 1));
+    }
+    return {Sa, Ladj, step_xy};
+}
+
+
 // get atom connectivity information
-std::tuple<std::vector<std::vector<int>>,std::vector<int>,std::vector<int>>
-FastAdjacentLists(int n_atom,  std::vector<std::vector<float>> Sa) {
+std::tuple<std::vector<std::vector<int>>, std::vector<int>, std::vector<int>>
+FastAdjacentLists(int n_atom, std::vector<std::vector<float>> Sa) {
     std::vector<std::vector<int>> Ladj;
     // ms row and column
     std::vector<int> w_ms_r;
@@ -50,12 +77,13 @@ FastAdjacentLists(int n_atom,  std::vector<std::vector<float>> Sa) {
         Ladj.push_back(Ladj_);
     }
     // w_ms_r and w_ms_c are used for traversing the first step
-    return std::make_tuple(Ladj,w_ms_r,w_ms_c);
+    return std::make_tuple(Ladj, w_ms_r, w_ms_c);
 }
 
 //  full step matrix implements
-std::vector<std::vector<float>>
-FastFullStepMatrix(int n_atom, std::vector<std::vector<float>> &Sa, std::vector<std::vector<int>> &Ladj,std::vector<int> w_ms_r,std::vector<int> w_ms_c) {
+/*std::vector<std::vector<float>>
+FastFullStepMatrix(int n_atom, std::vector<std::vector<float>> &Sa, std::vector<std::vector<int>> &Ladj,
+                   std::vector<int> w_ms_r, std::vector<int> w_ms_c) {
     std::vector<std::vector<float>> SF = Sa;
 
     // Iterate m from 1 to n_atom-2
@@ -81,9 +109,36 @@ FastFullStepMatrix(int n_atom, std::vector<std::vector<float>> &Sa, std::vector<
         w_ms_c = w_ms_c_;
     }
     return SF;
+}*/
+
+
+//  full step matrix implements
+std::vector<std::vector<float>>
+FastFullStepMatrix(int n_atom, std::vector<std::vector<float>> &Sa, std::unordered_map<int, std::set<int>> &Ladj,
+                   std::set<std::pair<int, int>> step_xy) {
+    std::vector<std::vector<float>> SF = Sa;
+
+    // Iterate m from 1 to n_atom-2
+    for (int m = 1; m < n_atom - 1; ++m) {
+        if (step_xy.empty()) {
+            break;
+        }
+        std::set<std::pair<int, int>> temp_step_xy{};
+        for (const auto &item: step_xy) {
+            int r = std::get<0>(item);
+            for (int c: Ladj[std::get<1>(item)]) {
+                if (SF[r - 1][c - 1] == 0 && r != c) {
+                    SF[r - 1][c - 1] = m + 1;
+                    temp_step_xy.insert(std::make_pair(r, c));
+                }
+            }
+        }
+        step_xy = temp_step_xy;
+    }
+    return SF;
 }
 
-std::vector<std::string> readMolFile(const std::string& filename) {
+std::vector<std::string> readMolFile(const std::string &filename) {
     std::ifstream file(filename);
     std::vector<std::string> lines;
     if (!file.is_open()) {
@@ -98,9 +153,9 @@ std::vector<std::string> readMolFile(const std::string& filename) {
     return lines;
 }
 
-vector <vector<float>> floyd(vector <vector<float>> MSa) {
+vector<vector<float>> floyd(vector<vector<float>> MSa) {
     int n = MSa.size();
-    vector <vector<float>> dis = MSa;
+    vector<vector<float>> dis = MSa;
     // Use transform and lambda expressions to replace all zeros with FLT_MAX
     std::transform(dis.begin(), dis.end(), dis.begin(),
                    [](std::vector<float> &row) {
@@ -124,12 +179,12 @@ vector <vector<float>> floyd(vector <vector<float>> MSa) {
     return dis;
 }
 
-std::vector<std::vector<float>> getMSFbyCSD(std::vector<std::string> mol_content){
+std::vector<std::vector<float>> getMSFbyCSD(std::vector<std::string> mol_content) {
     std::vector<std::vector<float>> SF;
     auto [n_atom, n_adj, n_atom_start] = betterFastStrFromMol(mol_content);
-    auto [Sa] = FastStepBondFromMol(n_atom, n_atom_start, n_adj, mol_content);
-    auto [Ladj,w_ms_r,w_ms_c] = FastAdjacentLists(n_atom,Sa);
-    SF = FastFullStepMatrix(n_atom, Sa, Ladj,w_ms_r,w_ms_c);
+    auto [Sa, Ladj, step_xy] = getMSaAndLadj(n_atom, n_atom_start, n_adj, mol_content);
+    //auto [Ladj, w_ms_r, w_ms_c] = FastAdjacentLists(n_atom, Sa);
+    SF = FastFullStepMatrix(n_atom, Sa, Ladj, step_xy);
     return SF;
 }
 
@@ -140,11 +195,12 @@ std::vector<std::vector<float>> getMSFbyFloydWarshall(const vector<std::string> 
     SF = floyd(Sa);
     return SF;
 }
+
 void printMSF(vector<std::vector<float>> &MSF) {
     for (const auto &item: MSF) {
         for (const auto &e: item) {
             cout << e << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
 }
--- cpp_code/common_utils/MsUtils.h
+++ cpp_code/common_utils/MsUtils.h
@@ -12,6 +12,8 @@
 #include <algorithm>
 #include <tuple>
 #include <ostream>
+#include <unordered_map>
+#include <set>
 
 namespace fs = std::filesystem;
 using namespace std;
@@ -24,17 +26,22 @@ std::tuple<int, int, int> betterFastStrFromMol(const std::vector<std::string> &I
 std::tuple<Matrix>
 FastStepBondFromMol(int n_atom, int n_atom_start, int n_adj, const std::vector<std::string> &ISI);
 
+// get the MSA and the atom connectivity
+std::tuple<Matrix, std::unordered_map<int, std::set<int>>, std::set<std::pair<int, int>>>
+getMSaAndLadj(int n_atom, int n_atom_start, int n_adj, const std::vector<std::string> &ISI);
+
 // generate the MSA faster
-std::tuple<std::vector<std::vector<int>>,std::vector<int>,std::vector<int>>
+std::tuple<std::vector<std::vector<int>>, std::vector<int>, std::vector<int>>
 FastAdjacentLists(int n_atom, std::vector<std::vector<float>> Sa);
 
 //  FullStepMatrix implements
 std::vector<std::vector<float>>
-FastFullStepMatrix(int n_atom, std::vector<std::vector<float>> &Sa, std::vector<std::vector<int>> &Ladj,std::vector<int> w_ms_r,std::vector<int> w_ms_c);
+FastFullStepMatrix(int n_atom, std::vector<std::vector<float>> &Sa, std::vector<std::vector<int>> &Ladj,
+                   std::set<std::pair<int, int>> step_xy);
 
 vector<vector<float>> floyd(vector<vector<float>> MSa);
 
-std::vector<std::string> readMolFile(const std::string& filename);
+std::vector<std::string> readMolFile(const std::string &filename);
 
 std::vector<std::vector<float>> getMSFbyCSD(std::vector<std::string> mol_content);
 
